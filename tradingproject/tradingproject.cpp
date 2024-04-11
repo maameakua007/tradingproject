c30ad239c2e45fa5d87ae0400ebb3692857ce8e0
nhyir <nhyir@NHYIRA8-PC>
2024-04-11 14:12:43 +0000

    Add project files.

---
+++ tradingproject/tradingproject.cpp
@@ -0,0 +1,19 @@
+// tradingproject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // 1 Print help
+
+    // 2 Print exchange stats
+
+    // 3 Make an offer 
+
+    // 4 Make a bid
+
+    // 5 Print wallet 
+
+    // 6 
+}
